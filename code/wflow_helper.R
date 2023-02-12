d0a0fecaa31930a06d83e6230177302eecded002
achiocch <andreas.chiocchetti@web.de>
2023-02-12 16:29:24 +0000

    wflow_publish(c("./analysis/*", "./docs/*", "./code/*"))

--- code/wflow_helper.R
+++ code/wflow_helper.R
@@ -1,5 +1,17 @@
 library(workflowr)
 
-workflowr::wflow_start("/files/scRNA_Qualitycheck/", existing=T)
+mode="run" # or setup
+if(mode=="setup"){
+  workflowr::wflow_start("/files/scRNA_Qualitycheck/", existing=T)  
+  wflow_git_config(user.name = "achiocch", user.email = "andreas.chiocchetti@web.de", overwrite=T)
+  wflow_git_remote("origin", 
+                   user = "KJPMolgenlab",
+                   repo  ="scSeq_UKW_2022", action="set_url")
+  
+  
+}
+
 wflow_build()
+
+wflow_publish(c("./analysis/*", "./docs/*","./code/*"))
 
