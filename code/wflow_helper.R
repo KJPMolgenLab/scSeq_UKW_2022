9decf3c5f486c4741cada13f7996675ca23b7ffa
Afsheen Kumar <afsheen.kumar@kgu.de>
2023-03-02 12:25:53 +0000

    workflowr::wflow_publish(c("./analysis/*", "./docs/*", "./code/*"))

--- code/wflow_helper.R
+++ code/wflow_helper.R
@@ -3,7 +3,7 @@ library(workflowr)
 mode="run" # or setup
 if(mode=="setup"){
   workflowr::wflow_start("/files/scRNA_Qualitycheck/", existing=T)
-  wflow_git_config(user.name = "achiocch", user.email = "andreas.chiocchetti@web.de", overwrite=T)
+  wflow_git_config(user.name = "SheenYo", user.email = "afsheen.yousaf@kgu.de", overwrite=T)
   wflow_git_remote("origin",
                    user = "KJPMolgenlab",
                    repo  ="scSeq_UKW_2022", action="set_url")
@@ -14,4 +14,5 @@ if(mode=="setup"){
 wflow_build()
 workflowr::wflow_publish(c("./analysis/*", "./docs/*","./code/*"))
 source("git push origin master")
+
 
