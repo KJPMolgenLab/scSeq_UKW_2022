c2ddc4966baface486794578ac128264e325efa1
achiocch <andreas.chiocchetti@web.de>
2023-06-25 15:17:21 +0000

    workflowr::wflow_publish(c("./analysis/*", "./docs/*", "./code/*"))

--- code/wflow_helper.R
+++ code/wflow_helper.R
@@ -3,7 +3,7 @@ library(workflowr)
 mode="run" # or setup
 if(mode=="setup"){
   workflowr::wflow_start("/files/scRNA_Qualitycheck/", existing=T)
-  wflow_git_config(user.name = "SheenYo", user.email = "afsheen.yousaf@kgu.de", overwrite=T)
+  wflow_git_config(user.name = "achiocch", user.email = "andreas.chiocchetti@web.de", overwrite=T)
   wflow_git_remote("origin",
                    user = "KJPMolgenlab",
                    repo  ="scSeq_UKW_2022", action="set_url")
@@ -11,13 +11,11 @@ if(mode=="setup"){
 
 }
 
-workflowr::wflow_build("./analysis/01_QC_scRNA.Rmd")
-workflowr::wflow_build("./analysis/02_Clustering_scRNA.Rmd")
-workflowr::wflow_build("./analysis/03_TrajectoryAnalysis_scRNA.Rmd")
-workflowr::wflow_build("./analysis/04_GenotypeAnalysis_scRNA.Rmd")
 
+workflowr::wflow_build("./analysis/*.Rmd")
 workflowr::wflow_publish(c("./analysis/*", "./docs/*","./code/*"))
-source("git push origin master")
+
+system("git push origin master")
 
 
 #git@github.com:KJPMolgenLab/scSeq_UKW_2022.git
